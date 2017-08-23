bc58d33241239ddd5c49b73d88b0e6a2be28349a
Takashi Kitajima <inc@2inc.org>
2017-08-23 09:33:07 +0900

    Static front page supports custom page template selecting

--- src/app/setup/front-page.php
+++ src/app/setup/front-page.php
@@ -11,8 +11,14 @@
  * @param string $template
  * @return string
  */
-add_filter( 'frontpage_template', function( $template ) {
-	if ( ! is_home() ) {
-		return $template;
-	}
-} );
+ add_filter( 'frontpage_template', function( $template ) {
+ 	if ( ! is_home() ) {
+ 		if ( $custom_page_template = get_post_meta(get_the_ID(), '_wp_page_template', true ) ) {
+ 			if ( $custom_page_template && 'default' !== $custom_page_template ) {
+ 				return $custom_page_template;
+ 			}
+ 		}
+
+ 		return $template;
+ 	}
+ } );
