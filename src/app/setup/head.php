d25b8fb9aaf59a90fcfbaeb0297f40d8b7cf9c96
Takashi Kitajima <inc@2inc.org>
2018-05-08 10:09:57 +0900

    Support iPhone X

--- src/app/setup/head.php
+++ src/app/setup/head.php
@@ -13,7 +13,7 @@
 add_action( 'wp_head', function() {
 	?>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, viewport-fit=cover">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
