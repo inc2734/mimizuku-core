d9b383dbf506832dc7a1bf39d76e7d3f43d346cb
Takashi Kitajima <inc@2inc.org>
2018-11-01 23:31:05 +0900

    Add load_theme_files()

--- src/Helper/load_theme_files.php
+++ src/Helper/load_theme_files.php
@@ -8,14 +8,14 @@
 namespace Inc2734\Mimizuku_Core\Helper;
 
 /**
- * get_template_part php files
+ * Include php files
  *
  * @SuppressWarnings(PHPMD.BooleanArgumentFlag)
  * @param string $directory
  * @param boolean $exclude_underscore
  * @return void
  */
-function get_template_parts( $directory, $exclude_underscore = false ) {
+function load_theme_files( $directory, $exclude_underscore = false ) {
 	if ( ! is_dir( $directory ) ) {
 		return;
 	}
@@ -23,11 +23,13 @@ function get_template_parts( $directory, $exclude_underscore = false ) {
 	$files = get_include_files( $directory, $exclude_underscore );
 
 	foreach ( $files['files'] as $file ) {
-		$template_name = str_replace( [ trailingslashit( get_template_directory() ), '.php' ], '', $file );
-		get_template_part( $template_name );
+		$file = str_replace( get_template_directory(), '', $file );
+		$file = str_replace( get_stylesheet_directory(), '', $file );
+		$file = get_theme_file_path( $file );
+		include_once( $file );
 	}
 
 	foreach ( $files['directories'] as $directory ) {
-		get_template_parts( $directory, $exclude_underscore );
+		load_theme_files( $directory, $exclude_underscore );
 	}
 }
