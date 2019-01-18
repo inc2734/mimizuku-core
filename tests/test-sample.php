5842324d975aa27675eda0fd1037a2eb9096c13f
Takashi Kitajima <inc@2inc.org>
2019-01-18 12:06:25 +0900

    Update inc2734/wp-view-controller

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -4,6 +4,7 @@ class Sample_Test extends WP_UnitTestCase {
 
 	public function setup() {
 		parent::setup();
+		new Inc2734\Mimizuku_Core\Core();
 	}
 
 	public function tearDown() {
@@ -12,9 +13,28 @@ class Sample_Test extends WP_UnitTestCase {
 
 	/**
 	 * @test
+	 * @runInSeparateProcess
 	 */
-	public function sample() {
-		new Inc2734\Mimizuku_Core\Helper();
-		new Inc2734\Mimizuku_Core\Core();
+	public function args() {
+		add_filter(
+			'mimizuku_get_template_part_args',
+			function( $args ) {
+				$args['slug'] = 'template2';
+				$args['name'] = 'name2';
+				$args['vars'] = [ 'key' => 'value2' ];
+				return $args;
+			}
+		);
+
+		add_action(
+			'inc2734_view_controller_get_template_part_pre_render',
+			function( $args ) {
+				$this->assertEquals( 'template2', $args['slug'] );
+				$this->assertEquals( 'name2', $args['name'] );
+				$this->assertEquals( 'value2', $args['vars']['key'] );
+			}
+		);
+
+		Inc2734\Mimizuku_Core\Helper::get_template_part( 'template', 'name', [ 'key' => 'value' ] );
 	}
 }
