9debd478b3c41658db6987c8010482f556ca4e12
Takashi Kitajima <inc@2inc.org>
2019-02-07 18:40:22 +0900

    Update inc2734/wp-view-controller

--- src/setup/view-controller.php
+++ src/setup/view-controller.php
@@ -22,9 +22,6 @@ add_filter(
 			'templates' => [
 				'vendor/inc2734/mimizuku-core/src/view',
 			],
-			'page-templates' => [
-				'page-templates',
-			],
 			'layout' => [
 				'templates/layout/wrapper',
 				'vendor/inc2734/mimizuku-core/src/view/templates/layout/wrapper',
