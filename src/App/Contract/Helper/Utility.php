f50ecd2d5a6bd56db9a148f1450adbc2e81fe1e7
Takashi Kitajima <inc@2inc.org>
2019-06-15 12:26:08 +0900

    Add Helper::get_taxonomies() and refactoring Helper::get_custom_post_types()

--- src/App/Contract/Helper/Utility.php
+++ src/App/Contract/Helper/Utility.php
@@ -18,12 +18,43 @@ trait Utility {
 	 * @return array
 	 */
 	public static function get_custom_post_types() {
-		return get_post_types(
+		$post_types = wp_cache_get( 'mimizuku-custom-post-types' );
+		if ( is_array( $post_types ) ) {
+			return $post_types;
+		}
+
+		$post_types = get_post_types(
 			[
 				'public'   => true,
 				'_builtin' => false,
 			]
 		);
+
+		wp_cache_set( 'mimizuku-custom-post-types', $post_types );
+		return $post_types;
+	}
+
+	/**
+	 * Return custom taxonomy names.
+	 * This method only works correctly after the init hook.
+	 *
+	 * @return array
+	 */
+	public static function get_taxonomies() {
+		$taxonomies = wp_cache_get( 'mimizuku-taxonomies' );
+		if ( is_array( $taxonomies ) ) {
+			return $taxonomies;
+		}
+
+		$taxonomies = get_taxonomies(
+			[
+				'public'   => true,
+				'_builtin' => false,
+			]
+		);
+
+		wp_cache_set( 'mimizuku-taxonomies', $taxonomies );
+		return $taxonomies;
 	}
 
 	/**
