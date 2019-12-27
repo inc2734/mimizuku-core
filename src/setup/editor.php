06f6e99141b77b628500640ba0e8e350f6d72a0d
Takashi Kitajima <inc@2inc.org>
2019-12-27 10:30:31 +0900

    Add inc2734/wp-custom-css-to-editor

--- src/setup/editor.php
+++ src/setup/editor.php
@@ -5,61 +5,6 @@
  * @license GPL-2.0+
  */
 
-use Inc2734\Mimizuku_Core\Helper;
+use Inc2734\WP_Custom_CSS_To_Editor;
 
-/**
- * Additional CSS to the block editor
- *
- * @return void
- */
-add_action(
-	'enqueue_block_editor_assets',
-	function() {
-		add_action(
-			'admin_head',
-			function() {
-				$css = wp_get_custom_css();
-				$css = str_replace( [ "\t", "\n", "\r" ], '', $css );
-				$css = preg_replace( '|\s*([\{\}])\s*|ms', '$1', $css );
-				$css = preg_replace( '/([^\{\}\(\)@]+?\{)/s', '.editor-styles-wrapper $1', $css );
-
-				if ( ! $css ) {
-					return;
-				}
-				?>
-				<style id="wp-additional-css">
-				<?php echo strip_tags( $css ); // WPCS XSS ok. ?>
-				</style>
-				<?php
-			}
-		);
-	}
-);
-
-/**
- * Additional CSS to the classic editor
- *
- * @param string $settings
- * @return string
- */
-add_filter(
-	'tiny_mce_before_init',
-	function( $mce_init ) {
-		$css = wp_get_custom_css();
-		$css = str_replace( [ "\t", "\n", "\r" ], '', $css );
-		$css = preg_replace( '|\s*([\{\}])\s*|ms', '$1', $css );
-		$css = preg_replace( '/([^\{\}\(\)@]+?\{)/s', '.mce-content-body.mceContentBody $1', $css );
-
-		if ( ! $css ) {
-			return $mce_init;
-		}
-
-		if ( ! isset( $mce_init['content_style'] ) ) {
-			$mce_init['content_style'] = '';
-		}
-
-		$mce_init['content_style'] .= addslashes( $css );
-		return $mce_init;
-	},
-	11
-);
+new WP_Custom_CSS_To_Editor\Bootstrap();
